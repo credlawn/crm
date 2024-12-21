9d52f31d83b9e935f79b32c8165556c4ed0813b4
credlawn <100140795+credlawn@users.noreply.github.com>
2024-12-21 20:04:46 +0530

    Basic Update

--- credlawn/credlawn/doctype/adobe_database/adobe_database.py
+++ credlawn/credlawn/doctype/adobe_database/adobe_database.py
@@ -2,17 +2,17 @@ from frappe.model.document import Document
 import frappe
 
 class AdobeDatabase(Document):
-    
+
     def after_insert(self):
         self.update_employee_name_and_decline_code()
-    
+
     def on_update(self):
         self.update_employee_name_and_decline_code()
         self.reload()
 
     def update_employee_name_and_decline_code(self):
         case_punching = frappe.db.get_all('Case Punching', filters={'reference_no': self.reference_no}, fields=['employee_name', 'mobile_no', 'case_source', 'email'])
-        
+
         if case_punching:
             case_punching_record = case_punching[0]
             frappe.db.set_value('Adobe Database', self.name, 'employee_name', case_punching_record.get('employee_name', 'Not Mapped'))
@@ -24,13 +24,11 @@ class AdobeDatabase(Document):
             frappe.db.set_value('Adobe Database', self.name, 'mobile_no', 'NA')
             frappe.db.set_value('Adobe Database', self.name, 'case_source', 'NA')
             frappe.db.set_value('Adobe Database', self.name, 'lead_owner', 'NA')
-        
+
         if self.decline_code:
             decline_code_data = frappe.db.get_value('Decline Code', filters={'name': self.decline_code}, fieldname=['final_remarks', 'sales_action_required'])
-            
-            
-            if decline_code_data:
+
+            if decline_code_data and final_stage != "Rejected":
                 final_remarks, sales_action_required = decline_code_data
                 frappe.db.set_value('Adobe Database', self.name, 'final_stage', final_remarks)
                 frappe.db.set_value('Adobe Database', self.name, 'action_required', sales_action_required)
-
